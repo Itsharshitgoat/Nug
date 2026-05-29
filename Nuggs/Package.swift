dc439c508168bf9b080a8946e582860983a1754b
Harshit <Itsharshitgoat@gmail.com>
2026-05-30 02:01:13 +0530

    Add files via upload

---
+++ Nuggs/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "Nuggs",
+    platforms: [
+        .macOS(.v12)
+    ],
+    products: [
+        .executable(name: "Nuggs", targets: ["Nuggs"])
+    ],
+    dependencies: [],
+    targets: [
+        .executableTarget(
+            name: "Nuggs",
+            dependencies: [],
+            path: "Nuggs",
+            resources: [
+                .copy("Resources")
+            ]
+        ),
+        .testTarget(
+            name: "NuggsTests",
+            dependencies: ["Nuggs"],
+            path: "Tests"
+        )
+    ]
+)
